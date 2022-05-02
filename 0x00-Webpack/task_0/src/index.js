34f1a7f2c855680a3c23023a2d9190bf6e060108
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-05-02 12:57:43 +0000

    update

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,7 @@
 import $  from "jquery";
 
-$('<p>Holberton Dashboard</p>').appendTo('body');
-$('<p>Dashboard data for the students</p>').appendTo('body');
-$('<p>Copyright - Holberton School</p>').appendTo('body');
+$(document).ready(() => {
+  $('<p>Holberton Dashboard</p>').appendTo('body');
+  $('<p>Dashboard data for the students</p>').appendTo('body');
+  $('<p>Copyright - Holberton School</p>').appendTo('body');
+});
