75bdbfd788d88ecee651086425edcaab3ba479e4
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-05-26 10:06:00 +0100

    update

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -5,6 +5,7 @@ export function concatElements(page1, page2) {
 }
 
 export function mergeElements(page1, page2) {
+  if (page1 == page2) return List(page2.valueSeq().toArray());
   const mergedobj = Map(page1).merge(Map(page2));
   return List(mergedobj.valueSeq().toArray());
 }
