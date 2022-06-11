f684ebbd0a7409ead65b3e7c00e1ae2afe89a644
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-06-11 14:27:45 +0100

    setup redux devtool extension differently

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import App from './App/App';
 import { uiReducer } from './reducers/uiReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(uiReducer, composeWithDevTools(applyMiddleware(thunk)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(uiReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
